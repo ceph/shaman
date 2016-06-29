c3fe77527e374191a0897c006369e8879409dbcc
Andrew Schoen <aschoen@redhat.com>
2016-06-29 10:10:35 -0500

    adds a Project model
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

---
+++ shaman/models/projects.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm.exc import DetachedInstanceError
+from shaman.models import Base
+
+
+class Project(Base):
+
+    __tablename__ = 'projects'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256), nullable=False, unique=True, index=True)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        try:
+            return '<Project %r>' % self.name
+        except DetachedInstanceError:
+            return '<Project detached>'
+
+    def __json__(self):
+        json_ = {}
+        json_["name"] = self.name
+        return json_
+
+
+def get_or_create(name, **kw):
+    project = Project.filter_by(name=name).first()
+    if not project:
+        project = Project(name=name)
+    return project
