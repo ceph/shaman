7121b3e50b9fcdf52de2d5d0228bdd3b800de921
Alfredo Deza <adeza@redhat.com>
2016-08-11 16:40:27 -0400

    controllers: create a refs controllers for repos
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ shaman/controllers/repos/refs.py
@@ -0,0 +1,24 @@
+from pecan import request, expose, abort
+from shaman.models import Project, Repo
+from shaman.controllers.repos import sha1s
+
+
+class RefController(object):
+
+    def __init__(self, ref_name):
+        self.ref_name = ref_name
+        self.project = Project.query.get(request.context['project_id'])
+        self.repos = Repo.query.filter_by(project=self.project, ref=ref_name).all()
+        request.context['ref'] = ref_name
+
+    @expose(generic=True, template='json')
+    def index(self):
+        abort(405)
+
+    @index.when(method='GET', template='json')
+    def index_get(self):
+        return [r.sha1 for r in self.repos]
+
+    @expose()
+    def _lookup(self, sha1_name, *remainder):
+        return sha1s.SHA1Controller(sha1_name), remainder
