7e22089a6fef0ab08923f72deddb4b41c1180939
Andrew Schoen <aschoen@redhat.com>
2016-08-24 14:46:23 -0500

    remove an unused import to appease flake8
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- shaman/models/archs.py
+++ shaman/models/archs.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 from shaman.models import Base
 
 
