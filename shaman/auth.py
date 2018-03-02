c98602f7e12aa97e49560338e316d6b4fd036043
Andrew Schoen <aschoen@redhat.com>
2018-03-02 13:25:59 -0600

    auth: add a decorator that checks auth on github webhooks
    This will fallback to basic_auth if github isn't the thing
    requesting the endpoint.
    
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- shaman/auth.py
+++ shaman/auth.py
@@ -1,4 +1,7 @@
 import base64
+import hmac
+
+from hashlib import sha1
 from pecan import request, abort, response, conf
 
 
@@ -12,6 +15,23 @@ def basic_auth():
         assert username == conf.api_user
         assert password == conf.api_key
     except:
+        response.headers['WWW-Authenticate'] = 'Basic realm="Shaman :: API"'
+        abort(401)
+
+    return True
+
+
+def github_basic_auth():
+    x_hub_signature = request.headers.get('X-Hub-Signature')
+    if not x_hub_signature:
+        # if this isn't github try basic_auth
+        return basic_auth()
+
+    github_secret = conf.github_secret
+    signature = "sha1={}".format(
+        hmac.new(github_secret, request.body, sha1).hexdigest()
+    )
+    if not hmac.compare_digest(x_hub_signature, signature):
         response.headers['WWW-Authenticate'] = 'Basic realm="Shaman :: API"'
         abort(401)
 
