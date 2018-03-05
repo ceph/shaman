59bbab4ba6b491fd79f075aeed0be3b9426ba5b9
Andrew Schoen <aschoen@redhat.com>
2018-03-05 15:15:14 -0600

    fix circular imports with util.publish_message and model listeners
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- shaman/models/__init__.py
+++ shaman/models/__init__.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine, MetaData, event
 from sqlalchemy.orm import scoped_session, sessionmaker, object_session, mapper
 from sqlalchemy.ext.declarative import declarative_base
 from pecan import conf
-from shaman.util import publish_message
 
 
 class _EntityBase(object):
@@ -67,6 +66,7 @@ def publish_build_message(mapper, connection, target):
     Send a message to RabbitMQ everytime a Build
     is updated
     """
+    from shaman.util import publish_message
     routing_key = "{}.builds".format(target.project.name)
     body = json.dumps(target.__json__())
     publish_message(routing_key, body)
@@ -77,6 +77,7 @@ def publish_repo_message(mapper, connection, target):
     Send a message to RabbitMQ everytime a Repo
     is updated
     """
+    from shaman.util import publish_message
     routing_key = "{}.repos".format(target.project.name)
     body = json.dumps(target.__json__())
     publish_message(routing_key, body)
