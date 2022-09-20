9c950e88b1d3e518a2cf143f43a5446c45d412ba
Rakshitha Kamath <rkamath@redhat.com>
2022-09-20 12:29:48 +0530

    This PR contains  [1] Changes to the script config/run.py .  [2] Changes that are required by the test suit for complete success.

--- shaman/tests/util.py
+++ shaman/tests/util.py
@@ -9,5 +9,4 @@ def make_credentials(correct=True, username=None, secret=None):
         creds = "%s:%s" % (username, secret)
     else:
         creds = 'you:wrong'
-    return 'Basic %s' % base64.b64encode(creds)
-
+    return 'Basic %s' % base64.b64encode(creds.encode()).decode()
