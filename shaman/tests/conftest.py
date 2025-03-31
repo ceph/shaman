aa1e769ee40c6352cdb23cfdf4be6e4b5e2fd25f
Zack Cerza <zack@cerza.org>
2025-03-31 15:02:33 -0600

    conftest: Rewrite fixtures to support sqlite
    So that we can run tests without having to set up infrastructure. This commit
    also adds a --db-url flag to py.test, so that the old postgres-specific behavior
    is still accessible by passing:
    
    --db-url 'postgresql+psycopg2://postgres@localhost:5432/shamantest'
    
    Signed-off-by: Zack Cerza <zack@cerza.org>

--- shaman/tests/conftest.py
+++ shaman/tests/conftest.py
@@ -1,28 +1,38 @@
+'''
+conftest.py
+
+By default, unit tests run against a SQLite3 database, as that requires no
+additional setup. To instead test against a PostgreSQL database, you might:
+
+podman run --rm -d -p 5432:5432 --name shamantest -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_DB=shamantest postgres:16
+py.test --db-url postgresql+psycopg2://postgres@localhost:5432/shamantest' ...
+
+Don't forget to remove the container when you are finished:
+podman rm -f shamantest
+'''
 import os
-from pecan.testing import load_test_app
-
-import subprocess
-
-from copy import deepcopy
-from pecan import conf
-from pecan import configuration
-from sqlalchemy import create_engine
-from sqlalchemy.pool import NullPool
-
-from shaman import models as _db
-from shaman.tests import util
 import pytest
 
-# needs running postgres, for example: podman run -d --rm -p 5432:5432 --name shamantest -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_DB=shamantest postgres:16
-DBNAME = 'shamantest'
-DBHOST = 'localhost'
-DBPORT = '5432'
-DBUSER = 'postgres'
-BIND = f"postgresql+psycopg2://{DBUSER}@{DBHOST}:{DBPORT}"
+from pecan import configuration, conf
+from pecan.testing import load_test_app
+
+from shaman import models
+from shaman.tests import util
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--db-url',
+        default='sqlite:///dev.db',
+        help='The database to use for testing. For more information see conftest.py'
+    )
+
+
+@pytest.fixture(scope='session')
+def db_url(pytestconfig):
+    return pytestconfig.getoption('db_url')
 
 
 class Factory(object):
-
     def __init__(self, **kw):
         for k, v in kw.items():
             setattr(self, k, v)
@@ -56,140 +66,36 @@ def config_file():
     return os.path.join(here, 'config.py')
 
 
-def reload_config():
-    from pecan import configuration
-    config = configuration.conf_from_file(config_file()).to_dict()
-
-    # Add the appropriate connection string to the app config.
-    config['sqlalchemy_w'] = {
-        'url': '%s/%s' % (BIND, DBNAME),
-        'encoding': 'utf-8',
-        'poolclass': NullPool
-    }
-
-    config['sqlalchemy_ro'] = {
-        'url': '%s/%s' % (BIND, DBNAME),
-        'encoding': 'utf-8',
-        'poolclass': NullPool
-    }
-
+@pytest.fixture(scope='function')
+def config(db_url):
+    print(db_url)
+    configuration.set_config(dict())
+    config_ = configuration.conf_from_file(config_file())
+    config_.sqlalchemy_w.url = db_url
+    config_.sqlalchemy_ro.url = db_url
     configuration.set_config(
-        config,
-        overwrite=True
-    )
-    _db.init_model()
-
-
-@pytest.fixture(scope='session')
-def app(request):
-    config = configuration.conf_from_file(config_file()).to_dict()
-
-    # Add the appropriate connection string to the app config.
-    config['sqlalchemy_w'] = {
-        'url': '%s/%s' % (BIND, DBNAME),
-        'encoding': 'utf-8',
-        'poolclass': NullPool
-    }
-
-    config['sqlalchemy_ro'] = {
-        'url': '%s/%s' % (BIND, DBNAME),
-        'encoding': 'utf-8',
-        'poolclass': NullPool
-    }
-    # Set up a fake app
-    app = TestApp(load_test_app(config))
-    return app
-
-
-@pytest.fixture(scope='session')
-def connection(app, request):
-    """Session-wide test database."""
-    # Connect and create the temporary database
-    print("=" * 80)
-    print("CREATING TEMPORARY DATABASE FOR TESTS")
-    print("=" * 80)
-    subprocess.call(['dropdb',
-                    '-h', DBHOST,
-                    '-p', DBPORT,
-                    '-U', DBUSER, DBNAME])
-    subprocess.call(['createdb',
-                    '-h', DBHOST,
-                    '-p', DBPORT,
-                    '-U', DBUSER, DBNAME])
-
-    # Bind and create the database tables
-    _db.clear()
-    engine_url = '%s/%s' % (BIND, DBNAME)
-
-    db_engine = create_engine(
-        engine_url,
-        encoding='utf-8',
-        poolclass=NullPool)
-
-    # AKA models.start()
-    _db.Session.bind = db_engine
-    _db.metadata.bind = _db.Session.bind
-
-    _db.Base.metadata.create_all(db_engine)
-    _db.commit()
-    _db.clear()
-
-    def teardown():
-        _db.Base.metadata.drop_all(db_engine)
-
-    request.addfinalizer(teardown)
-
-    # Slap our test app on it
-    _db.app = app
-    return _db
-
+        config_.to_dict(),
+        overwrite=True,
+     )
+    return conf
 
 @pytest.fixture(scope='function')
-def session(connection, request):
-    """Creates a new database session for a test."""
-    _config = configuration.conf_from_file(config_file()).to_dict()
-    config = deepcopy(_config)
-
-    # Add the appropriate connection string to the app config.
-    config['sqlalchemy_w'] = {
-        'url': '%s/%s' % (BIND, DBNAME),
-        'encoding': 'utf-8',
-        'poolclass': NullPool
-    }
-
-    config['sqlalchemy_ro'] = {
-        'url': '%s/%s' % (BIND, DBNAME),
-        'encoding': 'utf-8',
-        'poolclass': NullPool
-    }
-
-    connection.start()
+def app(request, config):
+    app = TestApp(load_test_app(config.to_dict()))
+    db_engine = conf.sqlalchemy_w.engine
+    models.start()
+    models.Base.metadata.create_all(db_engine)
+    models.Session.commit()
+    models.Session.flush()
+    models.clear()
 
     def teardown():
-        from sqlalchemy.engine import reflection
-
-        # Tear down and dispose the DB binding
-        connection.clear()
-
-        # start a transaction
-        engine = conf.sqlalchemy_w.engine
-        conn = engine.connect()
-        trans = conn.begin()
-
-        inspector = reflection.Inspector.from_engine(engine)
-
-        # gather all data first before dropping anything.
-        # some DBs lock after things have been dropped in
-        # a transaction.
-        conn.execute("TRUNCATE TABLE %s RESTART IDENTITY CASCADE" % (
-            ', '.join(inspector.get_table_names())
-        ))
-
-        trans.commit()
-        conn.close()
+        db_engine = conf.sqlalchemy_w.engine
+        models.rollback()
+        models.Base.metadata.drop_all(db_engine)
 
     request.addfinalizer(teardown)
-    return connection
+    return app
 
 
 class TestApp(object):
